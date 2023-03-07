f751883aa0317e5beca2f76c30cc29b2ed408201
Jude Boachie <judeboachiekwame@gmail.com>
2023-03-07 11:13:48 +0000

    Update README.md, add main file

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
-## coe-281-project
+#include <stdio.h>
 
-<small>Group project for the PROGRAMMING AND PROBLEM SOLVING class</small>
-
-# Story Game
-
+/*
 - Create a storyline which has various outcomes based on user-choices. (minimum of 5 outcomes)
 
 - Give user the ability to save game progress (hint: use files to store progress data)
 
 - Create player profiles so different players can play from their personal profiles.
+*/
+
